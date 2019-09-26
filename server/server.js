f338b3d24f8448bc37e2ed187230eb717ebfbe77
aki1997 <47532262+aki1997@users.noreply.github.com>
2019-09-26 11:23:26 +0530

    1.0.1

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 const path = require('path');
+const http= require('http');
 const express= require('express');
+const socketIO=require('socket.io');
 
-const publicPath=path.join(__dirname, '../public');
-const port=process.env.PORT || 3000;
-var app=express();
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
-    console.log(`Port ${port}`);
+io.on('connection', (socket) =>{
+    console.log('New User connected');
+  
+    socket.on('createMessage', (message) =>{
+     console.log('createMessage', message);
+     io.emit('newMessage', {
+         from:message.from,
+         text:message.text,
+         createAt:new Date().getTime()
+     });
+    });
+
+    socket.on('disconnect', ()=>{
+        console.log('Disconnect from server');
+    });
+});
+
+server.listen(port, () =>{
+console.log(`Server is up port ${port}`);
 });
