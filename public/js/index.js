7323dbc96b161eedaa3e8502006f8bb4996acb13
aki1997 <47532262+aki1997@users.noreply.github.com>
2019-09-26 13:16:05 +0530

    1.1.0

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message){
     console.log('newMessage', message);
+    var li=jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 })
+
+jQuery('#message-from').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from:'user',
+        text: jQuery('[name=message]').val()
+    }, function(){
+
+    });
+});
+
